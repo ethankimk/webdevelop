c74583f1a1ec6b4e7846a9b27e5fbcfba4237d2f
ethankim <ethankimk@gmail.com>
2018-10-15 20:44:17 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,36 @@
+//isEven
+/*
+function isEven(num) {
+	if(num % 2 === 0 ) {
+		//return true if even
+		return true;
+	}
+	
+	//return false otherwise
+	else {
+		return false
+	}
+	
+}*/
+/*function isEven(num){
+	retun num % 2 === 0;
+}*/
+
+/*function factorial(num) {
+	//define a result vvariable
+	var result = 1;
+	//caculate factorial and store value in result
+	for(var i = 2; i <= num; i++){
+		result *= i;
+	}
+	return result;
+}
+*/
+//factorisl (4)   = 4 x 3 x 2 x 1
+/*
+function kebabToSnake(str) {
+	//replace all '-' with "_"s
+	var newStr = str.replace(/-/g , "_");
+	return	newStr;
+
+}*/
